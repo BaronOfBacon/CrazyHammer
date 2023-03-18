3de0f3309edc39832e708a2919ba8c39e8435db0
BaronOfBacon <michael.belov@outlook.com>
2023-03-18 17:29:12 +0300

    LeoECS lite replaced with standard LeoECS. ECS unity-integration added. UniLeo added. ECSGameStartup added.

---
+++ Assets/Scripts/ECSGameStartup.cs
@@ -0,0 +1,61 @@
+
+using Leopotam.Ecs;
+using Leopotam.Ecs.UnityIntegration;
+using UnityEngine;
+using Voody.UniLeo;
+
+namespace CrazyHammer.Core
+{
+    public class ECSGameStartup : MonoBehaviour
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+        
+        private void Start()
+        {
+            _world = new EcsWorld();
+            
+            #if UNITY_EDITOR
+            EcsWorldObserver.Create(_world);
+            #endif
+            
+            _systems = new EcsSystems(_world);
+
+            _systems.ConvertScene();
+            
+            AddInjections();
+            AddOneFrames();
+            AddSystems();
+            
+            _systems.Init();
+        }
+
+        private void Update()
+        {
+            _systems.Run();
+        }
+
+        private void AddSystems()
+        {
+            
+        }
+
+        private void AddInjections()
+        {
+            
+        }
+        
+        private void AddOneFrames()
+        {
+            
+        }
+
+        private void OnDestroy()
+        {
+            _systems?.Destroy();
+            _systems = null;
+            _world?.Destroy();
+            _world = null;
+        }
+    }
+}
